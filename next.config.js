f071ad0d04ef23b9078bd3246b71be8f86b54fef
Marc Abramowitz <abramowi@adobe.com>
2021-12-17 10:54:56 -0800

    Add React Spectrum
    using instructions at https://github.com/adobe/react-spectrum/pull/2696

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const nextTranspileModules = require("next-transpile-modules");
+const getReactSpectrumModules = require("react-spectrum-monorepo/lib/getModules");
+
+// This wraps the Next.js configuration to do some transpilations so that React
+// Spectrum works in Next.js without any errors.
+//
+// Inspired by https://react-spectrum.adobe.com/react-spectrum/ssr.html#nextjs
+const withReactSpectrum = nextTranspileModules(getReactSpectrumModules());
+
+module.exports = withReactSpectrum({ reactStrictMode: true });
